48426f44d9124b31ca7232c168f0bef619103aa6
Mikhail Metrikin <metrikin@gmail.com>
2017-04-20 22:33:06 -0700

    Started Supervisor file

---
+++ bamazonSupervisor.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var Bamazon = require("./Bamazon.js");
+
+function BamazonSupervisor() {
+
+  if (!(this instanceof BamazonSupervisor)) {
+    return new BamazonSupervisor();
+  }
+  Bamazon.call(this);
+  this.prompt = [{
+    name: 'choice',
+    type: 'list',
+    message: 'What would you like to do',
+    choices: ['View Product Sales by Department', 'Create New Department', 'Quit']
+  }];
+
+}
+
+BamazonSupervisor.prototype =  Object.create(Bamazon.prototype);
+
+BamazonSupervisor.prototype.promptChoose = function () {
+  this.promptUser(this.prompt).then(function (response) {
+    switch(response.choice){
+
+      case 'View Product Sales by Department':
+        
+        break;
+
+      case 'Create New Department':
+
+        break;
+
+      case 'Quit':
+        console.log("GoodBye!");
+        this.endSession();
+        process.exit();
+        break;
+    }
+  })
+};
+
+// Begin run logic
+
+var supervisor = BamazonSupervisor();
+//supervisor.promptUser(supervisor.prompt)
